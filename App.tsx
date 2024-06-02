b37c54b9156b1eec96d469ff524f466c7580b311
Ali Aref <aliaref.js@gmail.com>
2024-06-02 14:24:34 +0430

    update

--- App.tsx
+++ App.tsx
@@ -1,21 +1,29 @@
 import React from "react";
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 import Button from "./components/ui/Button";
 import tw from "./tw";
 
 export default function App() {
   return (
-    <View style={tw`px-3 mt-5`}>
-      <StatusBar style="auto" />
-      <Text style={tw`font-bold text-2xl`}>Variants</Text>
-      <View style={tw`flex flex-row gap-2`}>
-        <Button variant="primary">Primary</Button>
-        <Button variant="info">Info</Button>
-        <Button variant="success">Success</Button>
-        <Button variant="warn">Warning</Button>
-        <Button variant="distructive">Error</Button>
-      </View>
+    <View
+      style={tw`flex flex-1 gap-2 items-center justify-center 
+				dark:bg-black`}
+    >
+      <Button label="Click me" variant="primary" onPress={()=>{
+				console.log("button pressed")
+			}} />
+      <Button label="Click me" variant="distructive" onPress={()=>{
+				console.log("button pressed")
+			}} />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,47 +1,67 @@
 import React from "react";
-import { View, Text, TouchableOpacity, TouchableNativeFeedback } from "react-native";
+import { Pressable, PressableProps, Text } from "react-native";
 import tw from "../../tw";
+import { ClassInput } from "twrnc";
 
-type ButtonProps = {
-  children: string;
-  variant?:
+type ButtonProps = PressableProps & {
+  label: string;
+  style?: ClassInput;
+  labelStyle?: ClassInput;
+  size?: "sm" | "md" | "lg";
+  variant:
     | "primary"
     | "secondary"
-    | "distructive"
-    | "success"
     | "info"
-    | "warn";
+    | "success"
+    | "wanring"
+    | "distructive"
+    | "link"
+    | "outline"
+    | "ghost";
 };
 
-const mapBgColor = {
-  primary: "bg-primary-500",
-  secondary: "#6c757d",
-  distructive: "#dc3545",
-  success: "#28a745",
-  info: "#17a2b8",
-  warn: "#ffc107",
+const variantStyles: Record<ButtonProps["variant"], string> = {
+  primary: "bg-primary",
+  secondary: "bg-distructive",
+  outline: "",
+  info: "",
+  success: "",
+  wanring: "",
+  distructive: "",
+  link: "",
+  ghost: "",
 };
-const mapTextColor = {
-  primary: "#fff",
-  secondary: "#fff",
-  distructive: "#fff",
-  success: "#fff",
-  info: "#fff",
-  warn: "#fff",
+const variantTextStyles: Record<ButtonProps["variant"], string> = {
+  primary: "text-white",
+  secondary: "text-white",
+  outline: "",
+  info: "",
+  success: "",
+  wanring: "",
+  distructive: "",
+  link: "",
+  ghost: "",
 };
 
-
-export default function Button({ variant, children }: ButtonProps) {
+export default function Button({
+  label,
+  variant,
+  style,
+  labelStyle,
+  ...props
+}: ButtonProps) {
   return (
-    <TouchableOpacity
+    <Pressable
       style={tw.style(
-        "px-3 py-2 rounded shadow-2xl shadow-black",
-        `bg-[${mapBgColor[variant ?? "primary"]}]`,
+        "px-3 py-2 rounded ",
+        variantStyles[variant],
+        style,
       )}
+      {...props}
     >
-      <Text style={tw.style(`text-[${mapTextColor[variant ?? "primary"]}]`)}>
-        {children}
+      <Text style={tw.style("", variantTextStyles[variant], labelStyle)}>
+        {label}
       </Text>
-    </TouchableOpacity>
+    </Pressable>
   );
 }
