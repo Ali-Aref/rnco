e4c8c88ed63fd6e543d466189a6b0cd846937542
Ali Aref <aliaref.js@gmail.com>
2024-07-01 15:17:02 +0430

    fixed(input): flex problems

--- components/ui/Input.tsx
+++ components/ui/Input.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { View, Text, TextInput, TextInputProps } from "react-native";
 import tw from "../../tw";
 
@@ -10,9 +10,18 @@ type InputProps = TextInputProps & {
   rightIcon?: React.ReactNode;
 };
 
-export default function Input({ label, description, error, leftIcon, rightIcon, ...props }: InputProps) {
+export default function Input({
+  label,
+  description,
+  error,
+  leftIcon,
+  rightIcon,
+  ...props
+}: InputProps) {
+  const [isActive, setisActive] = useState(false);
+
   return (
-    <View style={tw.style("flex flex-col w-full gap-2")}>
+    <View style={tw.style("w-full gap-2")}>
       {label && (
         <Text
           style={tw.style("text font-semibold", {
@@ -22,20 +31,26 @@ export default function Input({ label, description, error, leftIcon, rightIcon, 
           {label}
         </Text>
       )}
-      <View style={tw`vstack gap-1`}>
+      <View style={tw`gap-1`}>
         <View
           style={tw.style(
-            "hstack px-1 items-center rounded-md border-2 border-gray-200 dark:border-slate-600",
+            "flex-row px-1 pr-2 items-center rounded-md border-2 border-gray-200 dark:border-slate-600",
             {
+							"border-primary": isActive,
               "border-destructive dark:border-destructive": !!error,
             },
           )}
         >
-					{leftIcon && leftIcon}
-          <TextInput style={tw.style("text px-2 flex-1")} {...props} />
-					{rightIcon && rightIcon}
+          {leftIcon && leftIcon}
+          <TextInput
+            {...props}
+            onFocus={() => setisActive(true)}
+            onBlur={() => setisActive(false)}
+            style={tw.style("text px-2 flex-1")}
+          />
+          {rightIcon && rightIcon}
         </View>
-        {description && <Text style={tw`text`}>{description}</Text>}
+        {description && <Text style={tw`text text-gray-500`}>{description}</Text>}
         {error && <Text style={tw`text-destructive`}>{error}</Text>}
       </View>
     </View>
