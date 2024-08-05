d1811caf00532a1f003b131f00b472768e4425e5
Ali Aref <aliaref.js@gmail.com>
2024-08-05 15:36:57 +0430

    update(show-path): for routes that are not found

--- app/+not-found.tsx
+++ app/+not-found.tsx
@@ -1,13 +1,16 @@
-import { Link, Stack } from 'expo-router';
+import { Link, Stack, usePathname } from 'expo-router';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function NotFoundScreen() {
+	const path = usePathname()
+
   return (
     <>
       <Stack.Screen options={{ title: 'Oops!' }} />
       <View style={styles.container}>
         <Text style={styles.title}>This screen doesn't exist.</Text>
-
+				<Text>You are on route</Text>
+				<Text>{path}</Text>
         <Link href="/" style={styles.link}>
           <Text style={styles.linkText}>Go to home screen!</Text>
         </Link>
