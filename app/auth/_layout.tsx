2deb5f21dd9641b8886ec523de849b3bddd225fd
Ali Aref <aliaref.js@gmail.com>
2024-08-05 16:41:33 +0430

    update(animation): removed the logo animationa and add it to content

--- app/auth/_layout.tsx
+++ app/auth/_layout.tsx
@@ -1,9 +1,9 @@
 import LoginRegisterButton from '@/components/auth/LoginRegisterButton';
 import tw from '@/tw';
 import { Slot, usePathname } from 'expo-router';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { View, Text, Image, useColorScheme, ScrollView } from 'react-native';
-import Animated, { runOnJS, runOnUI, useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 
 const showBoxOnPaths: string[] = [
@@ -15,21 +15,21 @@ export default function AuthLayout() {
 	const path = usePathname()
 	const colorScheme = useColorScheme()
 	const boxOpacity = useSharedValue(1)
-	const logoTopPadding = useSharedValue<number>(40)
+	const contentTopMargin = useSharedValue<number>(0)
 
 	const logo = colorScheme === "dark"
-		? require(`../../assets/images/logo/dark.png`)
-		: require(`../../assets/images/logo/light.png`)
+		? require(`../../assets/images/logo/logo.png`)
+		: require(`../../assets/images/logo/logo.png`)
 
 	const animatedStyle = useAnimatedStyle(() => {
 		return {
 			opacity: boxOpacity.value,
 		};
 	});
-	const logoAnimatedStyle = useAnimatedStyle(()=>{
+	const logoAnimatedStyle = useAnimatedStyle(() => {
 		return {
-			marginTop: logoTopPadding.value,
-			marginBottom: -logoTopPadding.value,
+			marginTop: contentTopMargin.value,
+			marginBottom: -contentTopMargin.value,
 		}
 	})
 
@@ -40,7 +40,7 @@ export default function AuthLayout() {
 			{ duration: 500 },
 			(isFinished) => {
 				if (isFinished) {
-						logoTopPadding.value = withTiming(showBox ? 0: 50, { duration: 500 });
+					contentTopMargin.value = withTiming(showBox ? 0 : -80, { duration: 500 });
 				}
 			}
 		);
@@ -51,13 +51,15 @@ export default function AuthLayout() {
 
 	return (
 		<ScrollView style={tw`view`} contentContainerStyle={{ flexGrow: 1 }} overScrollMode='never' showsVerticalScrollIndicator={false}>
-			<Animated.View style={[tw`py-10 items-center`, logoAnimatedStyle]}>
-				<Image source={logo} resizeMode='stretch' style={{ width: 150, height: 150 }} />
-			</Animated.View>
+			<View style={[tw`py-10 items-center`]}>
+				<Image source={logo} resizeMode='stretch' style={{ width: 200, height: 200 }} />
+			</View>
 			<Animated.View style={[tw`mx-[15%] mb-5`, animatedStyle]}>
 				<LoginRegisterButton />
 			</Animated.View>
-			<Slot />
+			<Animated.View style={[logoAnimatedStyle]}>
+				<Slot />
+			</Animated.View>
 		</ScrollView>
 	);
 }
