220fa862457fe5fa8117524f774e3fd07b2e15c0
Ali Aref <aliaref.js@gmail.com>
2024-08-05 15:37:56 +0430

    added(pahtname): in the content

--- app/tabs1/index.tsx
+++ app/tabs1/index.tsx
@@ -1,11 +1,14 @@
 import tw from '@/tw';
+import { usePathname } from 'expo-router';
 import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function Tab1() {
+	const path = usePathname()
   return (
 		<View style={tw`view vstack w-full justify-center items-center`}>
 			<Text>Tabs 1</Text>
+			<Text>{path}</Text>
 		</View>
   );
 }
